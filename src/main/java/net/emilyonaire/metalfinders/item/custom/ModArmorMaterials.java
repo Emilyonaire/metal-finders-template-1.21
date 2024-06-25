7cef5331cd1722ed9efa95e610009d09c0878bda
Emilyonaire <emily.cowell.2002@gmail.com>
2024-06-26 00:48:42 +0100

    new modArmorMaterials.java implementation, using armorRestitched as reference with how to make them

--- src/main/java/net/emilyonaire/metalfinders/item/custom/ModArmorMaterials.java
+++ src/main/java/net/emilyonaire/metalfinders/item/custom/ModArmorMaterials.java
@@ -1,53 +1,50 @@
 package net.emilyonaire.metalfinders.item.custom;
 
+import net.emilyonaire.metalfinders.MetalFinders;
 import net.emilyonaire.metalfinders.item.ModItems;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ArmorMaterials;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Identifier;
 
-public class ModArmorMaterials implements ArmorMaterial {
-    private static final int[] BASE_DURABILITY = new int[] {13, 15, 16, 11};
-    private static final int[] PROTECTION_VALUES = new int[] {4, 7, 9, 4};
+import java.util.EnumMap;
+import java.util.List;
 
-    @Override
-    public int getDurability(EquipmentSlot slot) {
-        return BASE_DURABILITY[slot.getEntitySlotId()] * 40;
+public class ModArmorMaterials  {
+    public static final RegistryEntry<ArmorMaterial> RUBY = ModArmorMaterials.register("ruby", util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
+        map.put(ArmorItem.Type.BOOTS, 1);
+        map.put(ArmorItem.Type.LEGGINGS, 1);
+        map.put(ArmorItem.Type.CHESTPLATE, 1);
+        map.put(ArmorItem.Type.HELMET, 4);
+        map.put(ArmorItem.Type.BODY, 1);
+    }), 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0f, 0.0f, () Ingredient.ofItems((ModItems.RUBY),
+            List.of(new ArmorMaterial.Layer(new Identifier(MetalFinders.MOD_ID, "ruby"), "", false))));
+
+
+
+
+    public static RegistryEntry<ArmorMaterial> getDefault(Registry<ArmorMaterial> registry) {
+        return ArmorMaterials.LEATHER;
     }
 
-    @Override
-    public int getProtectionAmount(EquipmentSlot slot) {
-        return PROTECTION_VALUES[slot.getEntitySlotId()];
+    private static RegistryEntry<ArmorMaterial> register(String id, EnumMap<ArmorItem.Type, Integer> defense, int enchantability, RegistryEntry<SoundEvent> equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
+        List<ArmorMaterial.Layer> list = List.of(new ArmorMaterial.Layer(new Identifier(MetalFinders.MOD_ID, id)));
+        return ModArmorMaterials.register(id, defense, enchantability, equipSound, toughness, knockbackResistance, repairIngredient, list);
     }
 
-    @Override
-    public int getEnchantability() {
-        return 15;
-    }
-
-    @Override
-    public SoundEvent getEquipSound() {
-        return SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE;
-    }
-
-    @Override
-    public Ingredient getRepairIngredient() {
-        return Ingredient.ofItems(ModItems.RUBY);
-    }
-
-    @Override
-    public String getName() {
-        return "gilded_netherite";
-    }
-
-    @Override
-    public float getToughness() {
-        return 3.0F;
-    }
-
-    @Override
-    public float getKnockbackResistance() {
-        return 0.1F;
+    private static RegistryEntry<ArmorMaterial> register(String id, EnumMap<ArmorItem.Type, Integer> defense, int enchantability, RegistryEntry<SoundEvent> equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient, List<ArmorMaterial.Layer> layers) {
+        EnumMap<ArmorItem.Type, Integer> enumMap = new EnumMap<ArmorItem.Type, Integer>(ArmorItem.Type.class);
+        for (ArmorItem.Type type : ArmorItem.Type.values()) {
+            enumMap.put(type, defense.get(type));
+        }
+        return Registry.registerReference(Registries.ARMOR_MATERIAL, new Identifier(MetalFinders.MOD_ID, id),
+                new ArmorMaterial(enumMap, enchantability, equipSound, repairIngredient, layers, toughness, knockbackResistance));
     }
 }
